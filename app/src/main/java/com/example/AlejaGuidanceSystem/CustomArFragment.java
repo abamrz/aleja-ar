9a5e220c16ba9128167cbd914778bb81775db8a9
kaegi <kaegi.dev@gmail.com>
2020-09-22 16:38:30 +0200

    Remove plane controller

--- app/src/main/java/com/example/AlejaGuidanceSystem/CustomArFragment.java
+++ app/src/main/java/com/example/AlejaGuidanceSystem/CustomArFragment.java
@@ -10,7 +10,7 @@ public class CustomArFragment extends ArFragment {
 	@Override
 	protected Config getSessionConfiguration(Session session) {
 		// turning off hand-icon that appears on start-up.
-		getPlaneDiscoveryController().setInstructionView(null);
+		//getPlaneDiscoveryController().setInstructionView(null);
 		Config config = new Config(session);
 		// ensuring that update-listener is called whenever the camera frame updates.
 		config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
