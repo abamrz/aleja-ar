aaf83348e68e39f49d0467c3f3e0d1a8bd649c58
Lukas Metten <metten@in.tum.de>
2020-09-23 12:36:42 +0200

    NavigatioActivity AR preperation

--- app/src/main/java/com/example/AlejaGuidanceSystem/NavigationActivity.java
+++ app/src/main/java/com/example/AlejaGuidanceSystem/NavigationActivity.java
@@ -1,13 +1,66 @@
 package com.example.AlejaGuidanceSystem;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.AlejaGuidanceSystem.graph.Node;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.alg.DijkstraShortestPath;
+
+import java.util.Collection;
+
 
 public class NavigationActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
-    }
+	// AR
+	private CustomArFragment arFragment;
+	private ModelRenderable renderable;
+
+	// plan for the area
+	private Graph<Node, DefaultWeightedEdge> graph;
+
+	@Override
+	@SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_navigation);
+
+		arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+		arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+	}
+
+
+	/**
+	 * listener-method, called every time the camera frame is updated
+	 */
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	private void onUpdateFrame(FrameTime frameTime) {
+		// finding the current fragment of the scene
+		Session session = arFragment.getArSceneView().getSession();
+		Frame frame = arFragment.getArSceneView().getArFrame();
+		// all the items ARCore has tracked
+		Collection<AugmentedImage> images = frame.getUpdatedTrackables(AugmentedImage.class);
+
+		for(AugmentedImage image : images) {
+			if(image.getTrackingState() == TrackingState.TRACKING) {
+				// checking if correct image was detected
+				if(image.getName().equals("ar_pattern")) {
+					Log.d("NavigationActivity", "Image 'ar_pattern' was detected.");
+				}
+			}
+		}
+	}
 }
